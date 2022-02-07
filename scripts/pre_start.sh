34421b90cd1cc9dcf851f8a7345ba1c2d6676a4d
Abhishek sahu <58635182+abhi-dev91@users.noreply.github.com>
2022-02-07 09:57:20 +0530

    fixing/Buildtime

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
-apt-get install nodejs npm -y
-#npm install //to be uncomented if buildspec.yml config not worked.
+#apt-get update
+#apt-get install nodejs npm -y
+npm install #to be uncomented if buildspec.yml config not worked.
 npm install pm2 -g
