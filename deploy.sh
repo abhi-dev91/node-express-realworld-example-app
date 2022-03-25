812244daaaa48c0b579666c8b53dd9b5bea33f3f
Abhishek sahu <58635182+abhi-dev91@users.noreply.github.com>
2022-03-25 18:01:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ curl -sL https://deb.nodesource.com/setup_17.3.0 -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo npm install pm2 -g
-pm2 start app.js
+sudo pm2 start app.js
+sudo pm2 save
